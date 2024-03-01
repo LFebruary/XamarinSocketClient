9fc4b16ece504cb31c15fee44c4faa7be6821b81
Lyle February <lfebruary@icloud.com>
2024-03-01 14:49:51 +0200

    Cleanup

---
+++ XamarinClient/CustomDebug.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace XamarinClient
+{
+    /// <summary>
+    /// Provides additional methods to extend the debugging capabilities of <see cref="Debug"/>.
+    /// </summary>
+    /// <remarks>
+    /// These methods are provided in a separate class since as of writing C# does not support static extensions.
+    /// </remarks>
+    public static class CustomDebug
+    {
+        #region Package File Path Separator
+
+        private static string PackageSeparator { get; set; } = @"\XamarinClient\";
+
+        #endregion
+
+        #region WriteLine Extensions
+
+        /// <summary>
+        /// Writes a blank line to the trace listeners in the <see cref="System.Diagnostics.Debug.Listeners"/> collection.
+        /// </summary>
+        [Conditional("DEBUG")]
+        public static void WriteLine()
+        {
+            Debug.WriteLine(string.Empty);
+        }
+
+        /// <summary>
+        /// Writes a message preceded by callsite information to the trace listeners in the <see cref="System.Diagnostics.Debug.Listeners"/> collection.
+        /// </summary>
+        /// <param name="message">The message to write.</param>
+        /// <param name="addBlankLine">Whether to write a blank line after the written message.</param>
+        /// <param name="filePath">The file from which the method is called.</param>
+        /// <param name="caller"> The method in which the method is called.</param>
+        /// <param name="line">The line at which the method is called.</param>
+        [Conditional("DEBUG")]
+        public static void WriteLine(string message, bool addBlankLine = false, [CallerFilePath] string filePath = "", [CallerMemberName] string caller = "", [CallerLineNumber] int line = -1)
+        {
+            string fileString = filePath.Split(new string[] { PackageSeparator }, StringSplitOptions.None).Last();
+
+            Debug.WriteLine($"{fileString}: {caller}: line {line}: {message}");
+
+            if (addBlankLine)
+            {
+                WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Writes the value of the object's <see cref="object.ToString()"/> method preceded by callsite information to the trace listeners in the <see cref="System.Diagnostics.Debug.Listeners"/> collection.
+        /// </summary>
+        /// <param name="value">The object to write the string representation of.</param>
+        /// <param name="addBlankLine">Whether to write a blank line after the written message.</param>
+        /// <param name="filePath">The file from which the method is called.</param>
+        /// <param name="caller"> The method in which the method is called.</param>
+        /// <param name="line">The line at which the method is called.</param>
+        [Conditional("DEBUG")]
+        public static void WriteLine(object value, bool addBlankLine = false, [CallerFilePath] string filePath = "", [CallerMemberName] string caller = "", [CallerLineNumber] int line = -1)
+        {
+            WriteLine(value?.ToString() ?? "null", addBlankLine, filePath, caller, line);
+
+            if (addBlankLine)
+            {
+                WriteLine();
+            }
+        }
+        #endregion
+    }
+}
--- XamarinClient/MainPage.xaml.cs
+++ XamarinClient/MainPage.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -232,103 +229,6 @@ namespace XamarinClient
                 }
             }
         }
-        #endregion
-    }
-
-    /// <summary>
-    /// Provides additional methods to extend the debugging capabilities of <see cref="Debug"/>.
-    /// </summary>
-    /// <remarks>
-    /// These methods are provided in a separate class since as of writing C# does not support static extensions.
-    /// </remarks>
-    public static class CustomDebug
-    {
-        #region Package File Path Separator
-
-        private static string PackageSeparator { get; set; } = @"\XamarinClient\";
-
-        #endregion
-
-        #region WriteLine Extensions
-
-        /// <summary>
-        /// Writes a blank line to the trace listeners in the <see cref="System.Diagnostics.Debug.Listeners"/> collection.
-        /// </summary>
-        [Conditional("DEBUG")]
-        public static void WriteLine()
-        {
-            Debug.WriteLine(string.Empty);
-        }
-
-        /// <summary>
-        /// Writes a message preceded by callsite information to the trace listeners in the <see cref="System.Diagnostics.Debug.Listeners"/> collection.
-        /// </summary>
-        /// <param name="message">The message to write.</param>
-        /// <param name="addBlankLine">Whether to write a blank line after the written message.</param>
-        /// <param name="filePath">The file from which the method is called.</param>
-        /// <param name="caller"> The method in which the method is called.</param>
-        /// <param name="line">The line at which the method is called.</param>
-        [Conditional("DEBUG")]
-        public static void WriteLine(string message, bool addBlankLine = false, [CallerFilePath] string filePath = "", [CallerMemberName] string caller = "", [CallerLineNumber] int line = -1)
-        {
-            string fileString = filePath.Split(new string[] { PackageSeparator }, StringSplitOptions.None).Last();
-
-            Debug.WriteLine($"{fileString}: {caller}: line {line}: {message}");
-
-            if (addBlankLine)
-            {
-                WriteLine();
-            }
-        }
-
-        /// <summary>
-        /// Writes the value of the object's <see cref="object.ToString()"/> method preceded by callsite information to the trace listeners in the <see cref="System.Diagnostics.Debug.Listeners"/> collection.
-        /// </summary>
-        /// <param name="value">The object to write the string representation of.</param>
-        /// <param name="addBlankLine">Whether to write a blank line after the written message.</param>
-        /// <param name="filePath">The file from which the method is called.</param>
-        /// <param name="caller"> The method in which the method is called.</param>
-        /// <param name="line">The line at which the method is called.</param>
-        [Conditional("DEBUG")]
-        public static void WriteLine(object value, bool addBlankLine = false, [CallerFilePath] string filePath = "", [CallerMemberName] string caller = "", [CallerLineNumber] int line = -1)
-        {
-            WriteLine(value?.ToString() ?? "null", addBlankLine, filePath, caller, line);
-
-            if (addBlankLine)
-            {
-                WriteLine();
-            }
-        }
-
-        ///// <summary>
-        ///// Writes the string representation of the provided stream preceded by callsite information to the trace listeners in the <see cref="System.Diagnostics.Debug.Listeners"/> collection using a StreamReader.
-        ///// </summary>
-        ///// <remarks>
-        /////     <para>
-        /////         This method keeps track of the current value of the input stream's Position property and resets it after the stream has been read.
-        /////     </para>
-        /////     <para>
-        /////         Internally, this method copies the contents of the provided stream to a new MemoryStream, resets the original stream's Position property, and then creates and uses a StreamReader with the copied stream.
-        /////     </para>
-        ///// </remarks>
-        ///// <param name="stream">The stream to write the string representation of.</param>
-        ///// <param name="addBlankLine">Whether to write a blank line after the written message.</param>
-        ///// <param name="filePath">The file from which the method is called.</param>
-        ///// <param name="caller"> The method in which the method is called.</param>
-        ///// <param name="line">The line at which the method is called.</param>
-        //public static async Task WriteStream(Stream stream, bool addBlankLine = false, [CallerFilePath] string filePath = "", [CallerMemberName] string caller = "", [CallerLineNumber] int line = -1)
-        //{
-        //    using MemoryStream streamCopy = new MemoryStream();
-        //    long originalPosition = stream.Position;
-        //    await stream.CopyToAsync(streamCopy);
-
-        //    stream.Position = originalPosition;
-        //    streamCopy.Position = 0;
-
-        //    using StreamReader reader = new StreamReader(streamCopy);
-        //    WriteLine(await reader.ReadToEndAsync(), addBlankLine, filePath, caller, line);
-        //}
-
         #endregion
     }
 }
